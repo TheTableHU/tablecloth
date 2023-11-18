800dfce7d89e044f85d2b0d5d78668a2c2ae2c9d
Caleb St Clair <cstclair@harding.edu>
2023-11-17 20:09:06 -0600

    fix: change host if running in docker

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  host: 'http://localhost:8081',
+let host = 'http://localhost:8081';
+
+// Check if running inside a Docker container
+if (require('fs').existsSync('/proc/1/cgroup')) {
+  // Update the host for Docker environment
+  host = 'http://backend:8081'; // Use your Docker service name
 }
+
+module.exports = {
+  host,
+};
