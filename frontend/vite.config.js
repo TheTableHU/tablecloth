58db9ae63c29418e8152f147438026027677c2d3
Caleb St Clair <cstclair@harding.edu>
2023-12-08 11:12:10 -0600

    fix: eslint config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [react(), eslintPlugin({ cache: false })],
+  build: {
+    outDir: 'build'
+  },
+  plugins: [react()],
 });
