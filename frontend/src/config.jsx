f9b1ff54c81faecb66d8f2f814237d0fcb372646
cstclair@harding.edu <cstclair@harding.edu>
2023-12-22 19:39:16 -0600

    update default hostname and add scripts folder

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,4 +1,4 @@
-const host = import.meta.env.VITE_BACKEND_HOST || 'http://thetable.local:8081';
+const host = import.meta.env.VITE_BACKEND_HOST || 'http://tablecloth.local:8081';
 
 const config = {
   host: host,
---
+++ scripts/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/tablecloth
+
+docker compose -f docker-compose.prod.yml up -d
