d238b742ac51cbd3ebdbbf4657d0e0948bd0d7fc
cstclair@harding.edu <cstclair@harding.edu>
2024-01-29 10:51:07 -0600

    fix: script changes

--- scripts/backup_volume.sh
+++ scripts/backup_volume.sh
@@ -8,17 +8,22 @@ then
     exit 1
 fi
 
+# Set the working directory to the script's directory
+cd "$(dirname "$0")"
+
 # Backup Docker volume
 VOLUME_NAME="tablecloth_db"
 BACKUP_NAME="backup_$(date +%Y%m%d%H%M%S).tar.gz"
 
-docker run --rm -v $VOLUME_NAME:/data -v $(pwd):/backup busybox tar czvf /backup/$BACKUP_NAME -C /data
+# Backup Docker volume
+docker run --rm -v $VOLUME_NAME:/data -v $(pwd):/backup busybox tar czvf /backup/$BACKUP_NAME -C /data .
 
 # Upload the backup to Google Drive
 rclone copy $BACKUP_NAME gdrive:/tablecloth/backups
 
 # Delete the backup from the local machine
-rm $BACKUP_NAME
+# chmod 777 $BACKUP_NAME
+# rm $BACKUP_NAME
 
 # Keep only the last 7 backups on Google Drive
 rclone delete --min-age 7d gdrive:/tablecloth/backups
